0412ee8e9007e465f3d3af8540d17b1287c31e5f
Dylan Cook <dcook@email.neit.edu>
2023-08-30 14:18:09 -0400

    Completed other two algos and added an executable

--- Cook_VirtualMemorySimulation/src/main/java/cook/vramsim/Main.java
+++ Cook_VirtualMemorySimulation/src/main/java/cook/vramsim/Main.java
@@ -1,7 +1,6 @@
 package cook.vramsim;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -45,15 +44,17 @@ public class Main {
 
             System.out.println("The address %d contains: page number = %d offset = %d".formatted(addr, pageNum, offset));
         }
+        System.out.println("END PART 1 ----------------------------------------------------------------------------\n\n\n");
+
 
         Integer[][] fifo_vram = new Integer[100][frames];
         fifo_vram = PerformFIFOMemoryPolicy(fifo_vram, pages, frames);
 
         Integer[][] lfu_vram = new Integer[100][frames];
-        //lfu_vram = PerformLFUMemoryPolicy(lfu_vram, pages, frames);
+        lfu_vram = PerformLFUMemoryPolicy(lfu_vram, pages, frames);
 
         Integer[][] opra_vram = new Integer[100][frames];
-        //opra_vram = PerformOPRAMemoryPolicy(opra_vram, pages, frames);
+        opra_vram = PerformOPRAMemoryPolicy(opra_vram, pages, frames);
 
 
     }
@@ -62,6 +63,7 @@ public class Main {
         int[] timeSinceInserted = new int[frames];
         int faults = 0;
 
+        System.out.println("BEGIN FIFO POLICY ---------------------------------------------------------------------------------------------");
         for(int time = 0; time < 100; time++) {
 
             // Track page frequency for LFU using a hashmap
@@ -78,6 +80,7 @@ public class Main {
                         generatesFault = false;
                         memory[time] = Arrays.copyOf(memory[time - 1], memory[time - 1].length);
                         timeSinceInserted[i] = 0;
+                        System.out.println("Time at " + time + " has frames: " + Arrays.toString(memory[time]));
                         break;
                     }
                     else {
@@ -88,7 +91,6 @@ public class Main {
 
             if(generatesFault) {
                 faults++;
-                System.out.println("FIFO Algorithm produced a fault at time " + time + ".");
                 if(time == 0) {
                     memory[time][0] = pageToWrite;
                 }
@@ -118,7 +120,7 @@ public class Main {
                             }
                         }
 
-                        for (int i = 0; i < memory[time].length; i++) {
+                        for (int i = 0; i < frames; i++) {
                             if (i == oldestPageIndex)
                                 memory[time][oldestPageIndex] = pageToWrite;
                             else
@@ -128,25 +130,24 @@ public class Main {
                         //--------------------------------------------------------------------------------------------------------------------------
                     }
                 }
+                System.out.println("Time at " + time + " has frames: " + Arrays.toString(memory[time]));
             }
         }
-        System.out.println("FIFO Algorithm has completed and produced a total of " + faults + " page faults./n/n/n");
+        System.out.println("FIFO Algorithm has completed and produced a total of " + faults + " page faults.\n\n\n");
         return memory;
     }
 
     public static Integer[][] PerformLFUMemoryPolicy(Integer[][] memory, int[] pages, int frames) {
         int faults = 0;
-        HashMap<Integer, Integer> pageFrequencies = new HashMap<Integer, Integer>();
-
+        Hashtable<Integer, Integer> pageFrequencies = new Hashtable<Integer, Integer>();
+        System.out.println("BEGIN LFU POLICY ---------------------------------------------------------------------------------------------");
         for(int time = 0; time < 100; time++) {
-            // Track page frequency for LFU using a hashmap
             int pageToWrite = pages[time];
             if(pageFrequencies.containsKey(pageToWrite))
                 pageFrequencies.put(pageToWrite, (pageFrequencies.get(pageToWrite) + 1));
             else
                 pageFrequencies.put(pageToWrite, 0);
 
-            // Populate with -1 to know where empty values are w/o using null. Zero is default, but cannot be used as it is a possible page
             Arrays.fill(memory[time], -1);
 
             boolean generatesFault = true;
@@ -156,6 +157,7 @@ public class Main {
                     if (memory[time - 1][i] == pageToWrite) {
                         generatesFault = false;
                         memory[time] = Arrays.copyOf(memory[time - 1], memory[time - 1].length);
+                        System.out.println("Time at " + time + " has frames: " + Arrays.toString(memory[time]));
                         break;
                     }
                 }
@@ -163,7 +165,6 @@ public class Main {
 
             if (generatesFault) {
                 faults++;
-                System.out.println("FIFO Algorithm produced a fault at time " + time + ".");
                 if (time == 0) {
                     memory[time][0] = pageToWrite;
                 } else if (time < frames) {
@@ -176,21 +177,108 @@ public class Main {
                     }
                 }
                 else {
-                    int mostFrequentPage = pages[0];
-                    int mostFrequent = pageFrequencies.get(pages[0]);
-                    for(Integer freq : pageFrequencies.values()){
-                        if(freq > mostFrequent){
-                            mostFrequent = freq;
+                    Integer[] lastFrame = memory[time - 1];
+                    Integer[] associatedFreqs = new Integer[frames];
+
+                    for(int i = 0; i < lastFrame.length; i++){
+                        associatedFreqs[i] = pageFrequencies.getOrDefault(lastFrame[i], 0);
+                    }
+
+                    Integer maxFreq = 0;
+                    Integer maxFreqIndex = null;
+                    for(int i = 0; i < associatedFreqs.length; i++){
+                        if(associatedFreqs[i] > maxFreq){
+                            maxFreq = associatedFreqs[i];
+                            maxFreqIndex = i;
                         }
                     }
 
-
+                    for (int i = 0; i < frames; i++) {
+                        if (Integer.valueOf(i).equals(maxFreqIndex))
+                            memory[time][maxFreqIndex] = pageToWrite;
+                        else
+                            memory[time][i] = memory[time - 1][i];
+                    }
                 }
+                System.out.println("Time at " + time + " has frames: " + Arrays.toString(memory[time]));
             }
         }
+        System.out.println("LFU Algorithm has completed and produced a total of " + faults + " page faults.\n\n\n");
         return memory;
     }
     public static Integer[][] PerformOPRAMemoryPolicy(Integer[][] memory, int[] pages, int frames) {
+        int faults = 0;
+
+        System.out.println("BEGIN OPRA POLICY ---------------------------------------------------------------------------------------------");
+        for(int time = 0; time < 100; time++) {
+
+            // Track page frequency for LFU using a hashmap
+            int pageToWrite = pages[time];
+
+            // Populate with -1 to know where empty values are w/o using null. Zero is default, but cannot be used as it is a possible page
+            Arrays.fill(memory[time], -1);
+
+            boolean generatesFault = true;
+
+            if(time > frames) {
+                for (int i = 0; i < memory[time - 1].length; i++) {
+                    if (memory[time - 1][i] == pageToWrite) {
+                        generatesFault = false;
+                        memory[time] = Arrays.copyOf(memory[time - 1], memory[time - 1].length);
+                        System.out.println("Time at " + time + " has frames: " + Arrays.toString(memory[time]));
+                        break;
+                    }
+                }
+            }
+
+            if(generatesFault) {
+                faults++;
+                if(time == 0) {
+                    memory[time][0] = pageToWrite;
+                }
+                else if(time < frames) {
+                    memory[time] = Arrays.copyOf(memory[time - 1], memory[time - 1].length);
+                    for(int i = 0; i < frames; i++){
+                        if(memory[time][i] == -1){
+                            memory[time][i] = pageToWrite;
+                            break;
+                        }
+                    }
+                }
+                else {
+                    Hashtable<Integer, Integer> futureOccurances = new Hashtable<Integer, Integer>();
+                    Integer[] lastFrame = memory[time - 1];
+                    for(int p = 0; p < lastFrame.length; p++)
+                        for(int i = time; i < pages.length; i++) {
+                            if(pages[i] == lastFrame[p]){
+                                futureOccurances.put(lastFrame[p], i);
+                            }
+                        }
+
+                    Integer[] associatedFutureIndexes = new Integer[frames];
+
+                    for(int i = 0; i < lastFrame.length; i++){
+                        associatedFutureIndexes[i] = futureOccurances.getOrDefault(lastFrame[i], 0);
+                    }
+
+                    Integer maxFreqIndex = 0;
+                    for(int i = 0; i < associatedFutureIndexes.length; i++){
+                        if(associatedFutureIndexes[i] > maxFreqIndex){
+                            maxFreqIndex = i;
+                        }
+                    }
+
+                    for (int i = 0; i < frames; i++) {
+                        if (Integer.valueOf(i).equals(maxFreqIndex))
+                            memory[time][maxFreqIndex] = pageToWrite;
+                        else
+                            memory[time][i] = memory[time - 1][i];
+                    }
+                }
+                System.out.println("Time at " + time + " has frames: " + Arrays.toString(memory[time]));
+            }
+        }
+        System.out.println("OPRA Algorithm has completed and produced a total of " + faults + " page faults.\n\n\n");
         return memory;
     }
 }
